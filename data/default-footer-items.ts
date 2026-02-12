11549b07895e3dc5cbe172cbce8ac8ba36cae6e2
Luca Schöneberg <luca-schoeneberg@outlook.com>
2026-02-12 14:55:49 +0100

    Update default-footer-items.ts

--- data/default-footer-items.ts
+++ data/default-footer-items.ts
@@ -1,19 +1,13 @@
 import { FooterLink } from "@/components/nav/footer/footer";
 
 export const defaultFooterNavLinks: FooterLink[] = [
-    { href: "/faq", label: "Startseite" },
-    { href: "/brand", label: "Konfigurator" },
-    { href: "/fotos", label: "System" },
+    { href: "/", label: "Startseite" },
     { href: "/faq", label: "FAQ" },
     { href: "/brand", label: "Brand Assets" },
-    { href: "/fotos", label: "Fotos" },
-    { href: "/fotos", label: "Kontakt" },
+    { href: "/contact", label: "Kontakt" },
 ];
 
 export const defaultFooterMetaLinks: FooterLink[] = [
     { href: "/publisher", label: "Impressum" },
     { href: "/privacy", label: "Datenschutz" },
-    { href: "https://www.linkedin.com/company/marduofficial/", label: "LinkedIn" },
-    { href: "https://www.instagram.com/mardu.de", label: "Instagram" },
-    { href: "mailto:info@mardu.de", label: "Kontakt" },
 ];
